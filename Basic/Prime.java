3d97c3833f5022461bc8a8b9c88c11440ce6b803
Abhishek Kumar <115029976+Abhiscoder@users.noreply.github.com>
2025-06-21 13:26:24 +0000

    prime

---
+++ Basic/Prime.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Prime{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n = sc.nextInt();
+        if(n==2){
+            System.out.print("n is prime number"+5);
+        }else{
+            boolean isPrime = true;
+            for(int i=2; i<Math.sqrt(n); i++){
+                if(n%i ==0){
+                    isPrime=false;
+                }
+            }
+            if(isPrime== true){
+                System.out.println("N is prime number:"+n);
+            }else{
+                System.out.println("N is not prime number:"+n);
+            }
+        }
+    }
+}
